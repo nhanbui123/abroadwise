dc4868242d3c96fdacdd956df8b8c7a35245e7bd
nhanbui123 <45634190+nhanbui123@users.noreply.github.com>
2025-04-19 13:06:10 +0000

    add menu mobi and responsive

--- style.js
+++ style.js
@@ -1,16 +1,29 @@
 console.log("hello");
 
-document.querySelectorAll(".about-team").forEach(div=>{
+document.querySelectorAll(".about-team").forEach(div => {
     div.addEventListener('click', (event) => {
         document.querySelectorAll(".about-team").forEach(item => item.classList.remove("active"));
         event.currentTarget.classList.add("active");
         let idAbout = event.currentTarget.dataset.id;
 
-        document.querySelectorAll(".about-team-panel").forEach(panel=>{
+        document.querySelectorAll(".about-team-panel").forEach(panel => {
             panel.classList.remove("active");
-            if(panel.id == idAbout) {
+            if (panel.id == idAbout) {
                 panel.classList.add("active");
             }
         })
     })
+});
+
+
+const btnMenu = document.querySelector(".menu-mobi-icon");
+const mobiMenu = document.querySelector(".menu-mobile");
+console.log(btnMenu);
+btnMenu.addEventListener("click", function () {
+    mobiMenu.classList.toggle("active");
+    const icon = this.querySelector("i");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
+    // Toggle scroll
+    document.body.classList.toggle("no-scroll");
 })
